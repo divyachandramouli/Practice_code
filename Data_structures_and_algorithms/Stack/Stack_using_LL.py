e3aa35f9ffd019c9ba3ac1819786c649e44122e2
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-06-27 14:24:35 -0700

    Push and pop on stack object

--- Data_structures_and_algorithms/Stack/Stack_using_LL.py
+++ Data_structures_and_algorithms/Stack/Stack_using_LL.py
@@ -16,26 +16,34 @@ class LinkedList(object):
     	deleted=self.head
     	if self.head:
     		self.head=self.head.next
-    		deleted.next=None
+    		deleted.next=None 
+            # clears out the next field of the deleted element 
+            #(good practice, even though it's not in the specification)
     	return deleted
 
 class Stack(object):
     def __init__(self,top=None):
-        self.ll=Linkedlist(top)
+        self.ll=LinkedList(top)
 
     def push(self, new_element):
         self.ll.insert_first(new_element)
 
     def pop(self):
-        self.ll.delete_first(new_element)
+        return self.ll.delete_first()
 
 
 e1=Element(1)
 e2=Element(2)
 e3=Element(3)
+e4=Element(4)
 
 stack=Stack(e1)
 stack.push(e2)
 stack.push(e3)
 
-print (stack.pop().value)
+print(stack.pop().value)
+print(stack.pop().value)
+print(stack.pop().value)
+print(stack.pop())
+stack.push(e4)
+print(stack.pop().value)
