a4556721885f005504d49e6fdc494e858457083f
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-06-29 14:29:54 -0700

    Added comments for push/pop

--- Data_structures_and_algorithms/Stack/Stack_using_LL.py
+++ Data_structures_and_algorithms/Stack/Stack_using_LL.py
@@ -8,11 +8,12 @@ class LinkedList(object):
         self.head = head
 
     def insert_first(self,new_element):
+        "Insert new element as the head of the LinkedList"
     	new_element.next=self.head
     	self.head=new_element
 
     def delete_first(self):
-    	
+    	"Delete the first (head) element in the LinkedList as return it"
     	deleted=self.head
     	if self.head:
     		self.head=self.head.next
@@ -26,9 +27,11 @@ class Stack(object):
         self.ll=LinkedList(top)
 
     def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
         self.ll.insert_first(new_element)
 
     def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
         return self.ll.delete_first()
 
 
