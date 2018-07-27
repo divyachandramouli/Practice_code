61e9d70bac0b9a06455c962a491adb685c58c87c
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-07-27 14:17:36 -0700

    Fix errors in code

--- Data_structures_and_algorithms/3_Queue/Queue_using_arrays_size.py
+++ Data_structures_and_algorithms/3_Queue/Queue_using_arrays_size.py
@@ -10,27 +10,27 @@ class Queue:
 #Front and rear denote the indices 
 #Q is initialized to a list of size capacity with none as value
 
-	def isFull(self)
+	def isFull(self):
 		return self.size==self.capacity
 	def isEmpty(self):
 		return self.size==0
 
-	def enQ(self):
+	def EnQueue(self,item):
 		"Changes rear and size"
 		"You have an array of size n and an index i into that array."
 		"Increment i so that it points to the next position in the array,"
 		"but have i return to the first position once the end of the"
 		" array is reached. - circular increment using % operator"
-	
-		if(! (self.isFull()):
+
+		if(not (self.isFull())):
 			self.rear= (self.rear+1)%self.capacity
 			self.Q[self.rear]=item
 			self.size=self.size+1
 		else:
 			print("Queue is full!")
 
-	def deQ(self):
-		if (!self.isEmpty()):
+	def DeQueue(self):
+		if (not(self.isEmpty())):
 			dequeued=self.Q[self.front]
 			self.front=(self.front+1)%self.capacity
 			self.size=self.size-1
@@ -39,20 +39,19 @@ class Queue:
 			print("Queue is empty!")
 			return
 
+	 # Function to get rear of queue
+	def que_rear(self):
+		if self.isEmpty():
+	 		print("Queue is empty")
+	 		print("Rear item is",  self.Q[self.rear])
 
- # Function to get rear of queue
-    def que_rear(self):
-        if self.isEmpty():
-            print("Queue is empty")
-        print("Rear item is",  self.Q[self.rear])
-
-    #Function to get front of queue
+#Function to get front of queue
 	def que_front(self):
-        if self.isEmpty():
-            print("Queue is empty")
- 
-        print("Front item is", self.Q[self.front])
-         
+		if self.isEmpty():
+			print("Queue is empty")
+
+			print("Front item is", self.Q[self.front])
+
 queue = Queue(30)
 queue.EnQueue(10)
 queue.EnQueue(20)
